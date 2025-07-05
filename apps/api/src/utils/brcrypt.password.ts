8d59d0eda3624c994627cb666d203090680c2c31
huydat2001 <nguyenhuydatvp201@gmail.com>
2025-07-05 12:10:04 +0700

    feat(api): implement user management module with DTOs, controller, service, and schema

---
+++ apps/api/src/utils/brcrypt.password.ts
@@ -0,0 +1,8 @@
+import * as bcrypt from 'bcrypt';
+const saltRounds = 10;
+
+export async function hashPassword(password: string): Promise<string> {
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+}
