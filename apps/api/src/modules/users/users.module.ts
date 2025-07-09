0ae211eea16e8b6dcb618af94cce3a7b57e74c14
huydat2001 <nguyenhuydatvp201@gmail.com>
2025-07-09 16:35:03 +0700

    xoá các import bị trùng

--- apps/api/src/modules/users/users.module.ts
+++ apps/api/src/modules/users/users.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { JwtModule } from '@nestjs/jwt';
-import { User, UserSchema } from './schema/user.schema';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/user.schema';
 import { UploadImageModule } from '../upload/upload.module';
 
