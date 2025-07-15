6d1f687b6ff0dafa92a1f6b6210b92b8cb827f5a
namnp <procamboracho2002@gmail.com>
2025-07-15 07:51:48 +0700

    add next.config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
